8dc7beac8dfc800d1cb84b157cbb6bbe83dd14d4
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-08-11 20:07:05 -0700

    Cleaned up hour and day registration methods

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -54,31 +54,38 @@ end
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+
 hour_registered = []
 day_registered = []
+
+def sort_array_from_big_to_small(array)
+    array = array.tally
+    array = array.sort {|a,b| -(a[1]<=>b[1])}
+    array
+end
+
 contents.each do |row|
     id = row[0]
     name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
+    # Create the form letter (comment out to save from using Google api)
+    # zipcode = clean_zipcode(row[:zipcode])
     # legislators = legislators_by_zipcode(zipcode)
+    # form_letter = erb_template.result(binding)
+    # save_form_letter(id, form_letter) 
     
     phone_number = clean_phone_number(row[:homephone])
-    # puts phone_number
+    
+    # Find days and hours of where most users registered
     date = DateTime.strptime(row[:regdate], '%m/%d/%y %H:%M')
     hour_registered.push(date.hour)
     day_registered.push(date.wday)
-    # time = Time.parse(row[:regdate])
-    # puts time unless id == 4
-    # puts row[:regdate]
-    # form_letter = erb_template.result(binding)
-    # save_form_letter(id, form_letter) 
+
 end
-hour_registered = hour_registered.tally
-hour_registered = hour_registered.sort {|a,b| -(a[1]<=>b[1])}
-print hour_registered
+
 puts ''
-puts "Hour #{hour_registered[0][0]} had the most registrations with #{hour_registered[1][0]}, and #{hour_registered[2][0]} being the next most."
-day_registered = day_registered.tally
-day_registered = day_registered.sort {|a,b| -(a[1]<=>b[1])}
-print day_registered
-puts "Day #{day_registered[0][0]} had the most registrations with #{day_registered[1][0]}, and #{day_registered[2][0]} being the next most."
+hour_registered = sort_array_from_big_to_small(hour_registered)
+puts "Hour #{hour_registered[0][0]} had the most registrations with\
+ #{hour_registered[1][0]}, and #{hour_registered[2][0]} being the next most."
+day_registered = sort_array_from_big_to_small(day_registered)
+puts "#{Date::DAYNAMES[day_registered[0][0]]} had the most registrations with \
+#{Date::DAYNAMES[day_registered[1][0]]}, and #{Date::DAYNAMES[day_registered[2][0]]} being the next most."
